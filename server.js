d2336cbf0a9e7a19eca5cca63b149e7319ac3cae
Ambika <ambikula@gmail.com>
2019-04-16 13:56:49 -0700

    mongoDB Changed connection

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express =require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet');
 const productRoute = require('./routes/product');
+const key = require('./url');
 const app = express();
 const PORT =  8080;
 
-const mongoDB_URI = `mongodb+srv://Ambika:Dec%401986@cluster0-btzl5.mongodb.net/Lavu_products`;
+const mongoDB_URI = `mongodb+srv://${key.mongoDB_User}:${key.mongoDB_Pwd}@cluster0-btzl5.mongodb.net/${key.mongoDB_DataBase}`;
 app.use(bodyParser.json());
 app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,6 +16,8 @@ app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 })
+app.use(compression());
+app.use(helmet());
 app.use('/', productRoute);
 
 app.use((error, req, res, next)=>{
